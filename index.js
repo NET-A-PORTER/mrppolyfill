64c65003592b2f8d360280cea81042bd56b85ce3
Sabeur Thabti <sabeur.thabti@net-a-porter.com>
2015-08-18 15:57:32 +0100

    custom event

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('./polyfills/classList.js')();
 require('./polyfills/events.js')();
+require('./polyfills/customEvent.js')();
 require('./polyfills/requestAnimationFrame.js')();
 require('./polyfills/array.js');
 
